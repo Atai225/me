cf66f1e740df37ec2b08957531df56f9ced50c9b
Atai <atai.zhanabilov@gmail.com>
2022-07-07 16:31:24 +0600

    Me done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import img1 from './images/photo_2022-04-29_13-23-01.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <h3 className="name">Atai <br/> Zhana - <br/> Bilov</h3>
+
+        <div className="skrip__block">
+            <img src={img1} alt="" className="scrip__img-1" />
+            <span className="text-skrip">
+                atai <br/>
+                - <br/>
+                atai
+            </span>
+            <img src={img1} alt='' className="scrip__img-2" />
+        </div>
+        <div className="photos">
+            <h3 className="photos-title">Photos</h3>
+            <div className="photos__images">
+                <img src={img1} alt="" />
+                <img src={img1} alt="" />
+                <img src={img1} alt="" />
+                <img src={img1} alt="" />
+            </div>
+        </div>
+
+        <div className="char">
+            <h3 className="char3">Характерстики</h3>
+            <ul className="ch__list">
+                <li className="ch__item">
+                    <p className="ch__name">Харизма</p>
+                    <p className="ch__name">Ебал харизму</p>
+                </li>
+                <li className="ch__item">
+                    <p className="ch__name">Внешность</p>
+                    <p className="ch__name">Гига Чад</p>
+                </li>
+                <li className="ch__item">
+                    <p className="ch__name">Тело</p>
+                    <p className="ch__name">
+                         Шварцнеггер в ахуе<br/>
+                    </p>
+                </li>
+                <li className="ch__item">
+                    <p className="ch__name">Мозги</p>
+                    <p className="ch__name">Работают</p>
+                </li>
+                <li className="ch__item">
+                    <p className="ch__name">Доброта</p>
+                    <p className="ch__name">Э не нахуй</p>
+                </li>
+                <li className="ch__item">
+                    <p className="ch__name">Милота</p>
+                    <p className="ch__name">100%</p>
+                </li>
+                <li className="ch__item">
+                    <p className="ch__name">Защита</p>
+                    <p className="ch__name">Топ дифендер</p>
+                </li>
+            </ul>
+        </div>
+
+        <div className="pj">
+            <h3 className="pj__title">Пожелание от разработчика</h3>
+            <h3 className="pj__title2">НЕ СДО-<br/>ХНИ РАНЬ-<br/>ШЕ ВРЕ-<br/>МЕНИ</h3>
+            <img src={img1} alt=""/>
+        </div>
+    </div>
     </div>
   );
 }
